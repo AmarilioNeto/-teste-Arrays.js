ec7b658e6c9bd8152d59f93b9fd026a66ea328aa
AmarilioNeto <amarilioneto.an@gmail.com>
2021-03-19 22:21:02 -0300

    Arrays.js 7

--- Arrays.js
+++ Arrays.js
@@ -30,36 +30,4 @@ console.log(carros);
 
 carros.push(123);
 console.log(carros);
-=======
-var carros = ["Gol", "Palio","Uno","ka"];
-console.log (carros);
 
-var primeiroCarro = carros[0];
-var segundoCarro = carros[1];
-var terceiroCarro = carros[2];
-var quartoCarro = carro[3 ]
-console.log("primeira posição: " + primeiroCarro);
-console.log("segunda posição: " + segundoCarro);
-console.log("terceira posição: " + terceiroCarro);
-console.log("quarta posição: "=  + quarto Carro);
-carros[2] = "argo";
-console.log(carros[2]);
-
-console.log(terceiroCarro);
-
-console.log("tamanho do Array: " + carros.length);
-
-console.log("ultimo elento do arry:" + carros[carros.length - 1]);
-
-carros[3] = "sandero";
-console.log("ultimo elento do arry:" + carros[carros.length - 1]);
-
-carros[carros.length] = "honda fit";
-console.log("ultimo elento do arry:" + carros[carros.length - 1]);
-
-carros.push("polo");
-console.log(carros);
-
-carros.push(123);
-console.log(carros);
->>>>>>> 5f9cbbdb780e70a894c115af34a31e0f25f295f0
