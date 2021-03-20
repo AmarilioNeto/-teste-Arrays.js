4f94c4f0ef0fb2a5240f1d0cdb27f3f4d6aa7460
AmarilioNeto <80914219+AmarilioNeto@users.noreply.github.com>
2021-03-19 21:05:34 -0300

    Add files via upload

--- Arrays.js
+++ Arrays.js
@@ -1,13 +1,14 @@
-var carros = ["Gol", "Palio","Uno"];
+var carros = ["Gol", "Palio","Uno","ka"];
 console.log (carros);
 
 var primeiroCarro = carros[0];
 var segundoCarro = carros[1];
-var terceiroCarro = carros[2]
+var terceiroCarro = carros[2];
+var quarto Varro = carro[3];
 console.log("primeira posição: " + primeiroCarro);
 console.log("segunda posição: " + segundoCarro);
 console.log("terceira posição: " + terceiroCarro);
-
+console.log("quarta posição: "=  + quarto Carro);
 carros[2] = "argo";
 console.log(carros[2]);
 
